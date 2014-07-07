74ad27328bd6c6ebf588450a0e568a2416f923f1
deepujohn <deepujohn.ideology@gmail.com>
2014-07-07 15:40:38 +0530

    CRUD without validation

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,14 +1,37 @@
 class EmployeesController < ApplicationController
 	def index
-		#@employees=Employee.All
+		@employees=Employee.all
 	end
 	def show
-		#@employee=Employee.find(params[:id])
+		@employee=Employee.find(params[:id])
 	end
 	def new
 		@employee=Employee.new
 	end
-
+	def create
+		@employee=Employee.new(employee_params)
+		if @employee.save
+			redirect_to @employee
+		else
+			render 'new'
+		end
+	end
+	def edit
+		@employee=Employee.find(params[:id])		
+	end
+	def update
+		@employee=Employee.find(parms[:id])
+		if @employee.update(employee_params)
+			redirect_to @employee
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@employee=Employee.find([:id])
+		@employee.destroy
+		redirect_to articles_path
+	end
 	private
 	def employee_params
 		params.require(:employee).permit(:first_name,:middle_name,:last_name,:email,:password,:confirm_password,:date_of_birth,:city,:address,:gender,:language,:phone,:pincode)
