4d810bd6329e1c715c175adb8bc19d93bae8ae24
deepujohn <deepujohn.ideology@gmail.com>
2014-07-07 17:35:40 +0530

    model presence changed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,3 @@
 class Employee < ActiveRecord::Base
-	validates_presence_of :first_name, :last_name, :email, :password, :confirm_password, :date_of_birth, :city, :address, :gender, :language, :phone, :pincode
+	validates :first_name, :last_name, :email, :password, :confirm_password, :date_of_birth, :city, :address, :gender, :language, :phone, :pincode, presence:true
 end
